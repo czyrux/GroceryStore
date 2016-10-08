6dd775b8c4580e8f375967b5a48396637387fac8
czyrux <agutierrez88s@gmail.com>
2016-10-08 21:33:52 +0200

    New set of products

--- app/src/main/java/de/czyrux/store/core/data/sources/CartProviderScaffolding.java
+++ app/src/main/java/de/czyrux/store/core/data/sources/CartProviderScaffolding.java
@@ -1,19 +1,22 @@
 package de.czyrux.store.core.data.sources;
 
+import java.util.List;
+
 import de.czyrux.store.core.domain.cart.Cart;
 import de.czyrux.store.core.domain.cart.CartBuilder;
 import de.czyrux.store.core.domain.cart.CartProductFactory;
 import de.czyrux.store.core.domain.product.Product;
 
-public class CartProviderScaffolding {
+class CartProviderScaffolding {
 
     public static Cart getACartWithProducts() {
-        Product product = new Product("sku1", "Nike Performance", "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcStw-eAC6L_-M25bqzfnQqGwsJVC39yTvDQ3qON6qVDtzX0iPEJ&usqp=CAY", 84.95);
-        Product product2 = new Product("sku2", "Adidas Originals", "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcSDmpXJE6EtdkH80VLoPl3QZiILZeZu87Bh0a00sE1f7AtrpQjQSOJXuP6QnoatXZVGGSGFJZDv&usqp=CAE", 99.95);
+        List<Product> products = ProductProviderScaffolding.getProductList();
 
-        return CartBuilder.empty()
-                .addProduct(CartProductFactory.newCartProduct(product, 1))
-                .addProduct(CartProductFactory.newCartProduct(product2, 2))
-                .build();
+        CartBuilder cartBuilder = CartBuilder.empty();
+        for (Product product : products) {
+            cartBuilder.addProduct(CartProductFactory.newCartProduct(product, 1));
+        }
+
+        return cartBuilder.build();
     }
 }
--- app/src/main/java/de/czyrux/store/core/data/sources/InMemoryProductDataSource.java
+++ app/src/main/java/de/czyrux/store/core/data/sources/InMemoryProductDataSource.java
@@ -1,10 +1,9 @@
 package de.czyrux.store.core.data.sources;
 
-import java.util.Arrays;
 import java.util.List;
 
-import de.czyrux.store.core.domain.product.ProductDataSource;
 import de.czyrux.store.core.domain.product.Product;
+import de.czyrux.store.core.domain.product.ProductDataSource;
 import rx.Observable;
 
 public class InMemoryProductDataSource implements ProductDataSource {
@@ -12,8 +11,7 @@ public class InMemoryProductDataSource implements ProductDataSource {
     @Override
     public Observable<List<Product>> getAllCatalog() {
         return Observable.defer(() -> {
-            List<Product> products = Arrays.asList(new Product("sku1", "Nike Performance", "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcStw-eAC6L_-M25bqzfnQqGwsJVC39yTvDQ3qON6qVDtzX0iPEJ&usqp=CAY", 84.95),
-                    new Product("sku2", "Adidas Originals", "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcSDmpXJE6EtdkH80VLoPl3QZiILZeZu87Bh0a00sE1f7AtrpQjQSOJXuP6QnoatXZVGGSGFJZDv&usqp=CAE", 99.95));
+            List<Product> products = ProductProviderScaffolding.getProductList();
             return Observable.just(products);
         });
     }
